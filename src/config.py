2985b376e5d6bb714368099c83276332d2c3572b
patoobyte <ake.leesy@gmail.com>
2026-07-01 16:22:26 +0200

    Preprocessing done

--- src/config.py
+++ src/config.py
@@ -1,9 +1,125 @@
 # src/config.py
 from pathlib import Path
 
+## ===== PATHS =====
 ROOT = Path(__file__).resolve().parent.parent
 DATA_DIR = ROOT / "data"
 DATA_RAW = ROOT / "data" / "raw" / "listings_raw_20260629.csv"
 DATA_AUDITED = ROOT / "data" / "raw" / "listings_raw_20260629_audited.csv"
 DATA_CLN = ROOT / "data" / "clean" / "listings_clean.csv"
+
+## ===== FEATURES CLASSIFICATION =====
+NUMERIC_FEATURES = [
+    "latitude",
+    "longitude",
+    "bedrooms",
+    "livable_surface",
+    "bathrooms",
+    "toilets",
+    "floor",
+    "build_year",
+    "facades",
+    "garden_surface",
+    "terrace_surface",
+    "garages",
+    "indoor_parking",
+    "outdoor_parking",
+    "number_of_floors",
+    "total_land_surface",
+    "nursery_nearest_walk_m",
+    "preschool_nearest_walk_m",
+    "elementary_school_nearest_walk_m",
+    "high_school_nearest_walk_m",
+    "bus_stop_nearest_walk_m",
+    "tram_stop_nearest_walk_m",
+    "train_station_nearest_walk_m",
+    "train_station_nearest_drive_m",
+    "motorway_nearest_drive_m",
+    "airport_nearest_drive_m",
+    "charging_station_nearest_drive_m",
+    "supermarket_nearest_walk_m",
+    "supermarket_count",
+    "car_sharing_nearest_walk_m",
+    "bike_sharing_nearest_walk_m",
+    "post_year",
+    "post_month",
+]
+
+BINARY_FEATURES = [
+    "new_construction",
+    "vat",
+    "currently_leased",
+    "furnished",
+    "heat_pump",
+    "solar_panels",
+    "air_conditioning",
+    "electrical_certificate",
+    "garden",
+    "terrace",
+    "garage",
+    "cellar",
+    "swimming_pool",
+    "elevator",
+    "access_for_disabled",
+    "entry_phone",
+    "attic",
+    "alarm",
+    "security_door",
+    "rain_water_tank",
+    "fireplace",
+    "electric_charging_station",
+    "hammam_sauna_jacuzzi",
+    "domotica",
+    "available_immediately",
+]
+
+CATEGORICAL_FEATURES = [
+    "property_type",
+    "province",
+    "region",
+    "kitchen_equipment",
+    "building_state",
+    "heating_type",
+    "glazing_type",
+    "facade_orientation",
+    "terrace_orientation",
+    "flooding_area_clean",
+    "epc_quality",
+    "postal_code",
+    "category",
+]
+
+## ===== MISSING HANDLING FOR BINARY FEATURES =====
+BINARY_MISSING_AS_ZERO = [
+    "heat_pump",
+    "solar_panels",
+    "air_conditioning",
+    "garden",
+    "terrace",
+    "garage",
+    "cellar",
+    "swimming_pool",
+    "elevator",
+    "access_for_disabled",
+    "entry_phone",
+    "attic",
+    "alarm",
+    "security_door",
+    "rain_water_tank",
+    "fireplace",
+    "electric_charging_station",
+    "hammam_sauna_jacuzzi",
+    "domotica",
+]
+
+BINARY_MISSING_AS_UNKNOWN = [
+    "new_construction",
+    "vat",
+    "currently_leased",
+    "furnished",
+    "electrical_certificate",
+    "available_immediately",
+]
+
+## ===== MISC =====
 RANDOM_SEED = 13
---
+++ src/features_engineer.py
@@ -0,0 +1,210 @@
+import pandas as pd
+import config
+from sklearn.model_selection import train_test_split
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
+from sklearn.linear_model import LinearRegression
+
+## SETUP
+def load_data():
+    df = pd.read_csv(config.DATA_CLN)
+    print("Loaded CSV")
+    return df
+
+def remove_exact_duplicates(df):
+    duplicate_rows = df.duplicated(keep="first")
+    df_deduped = df.loc[~duplicate_rows].copy()
+    print("Removed exact duplicates")
+    return df_deduped
+
+## Feature engineering
+## "availability" = turn into binary immediately_available
+# "category"
+#     "postal_code",
+#     "epc",
+#     "flooding_area_type",
+def engineer_features(df):
+    df = process_availability(df)
+    df = process_category(df)
+    df = process_epc(df)
+    df = process_flooding_area(df)
+    df = process_binary_missing_values(df)
+    return df
+
+# Process "availability" to a binary "available_immediately"
+def process_availability(df):
+    df = df.copy()
+
+    # Converts all value for smooth comparison
+    availability_clean = (
+    df["availability"]
+    .astype("string")
+    .str.lower()
+    .str.strip()
+    )
+
+    # New column created with missing as base value
+    df["available_immediately"] = pd.NA
+
+    # If availability == immediately, set to 1
+    df.loc[availability_clean.eq("immediately"), "available_immediately"] = 1
+
+    # If availability is != immediately, set to 0
+    df.loc[availability_clean.notna() & ~availability_clean.eq("immediately"), "available_immediately"] = 0
+    
+    # Converts to Int64 to preserve missing data
+    df["available_immediately"] = df["available_immediately"].astype("Int64")
+
+    # Drop original column
+    df = df.drop(columns=["availability"])
+
+    print("Processed feature 'availability' into 'available_immediately'")
+    return df
+
+def process_category(df):
+    df = df.copy()
+
+    categories_to_remove = [
+        "garage",
+        "investment_property",
+        "land",
+        "entreprise",
+    ]
+
+    # Remove unwanted categories
+    df = df[~df["category"].isin(categories_to_remove)].copy()
+
+    # Transform student_house to appartment
+    df["category"] = df["category"].replace({
+        "student_house": "appartment"
+    })
+
+    print("Processed features 'category' & 'property_type'")
+    return df
+
+def process_epc(df):
+    df = df.copy()
+
+    epc_map = {
+        # Flanders
+        "FlandersDoubleA": "excellent",
+        "FlandersSingleA": "excellent",
+        "FlandersB": "good",
+        "FlandersC": "poor",
+        "FlandersD": "poor",
+        "FlandersE": "bad",
+        "FlandersF": "bad",
+
+        # Brussels
+        "BrusselsA": "excellent",
+        "BrusselsB": "good",
+        "BrusselsC": "good",
+        "BrusselsD": "poor",
+        "BrusselsE": "poor",
+        "BrusselsF": "bad",
+        "BrusselsG": "bad",
+
+        # Wallonia
+        "WalloniaTripleA": "excellent",
+        "WalloniaDoubleA": "excellent",
+        "WalloniaSingleA": "good",
+        "WalloniaB": "good",
+        "WalloniaC": "poor",
+        "WalloniaD": "poor",
+        "WalloniaE": "poor",
+        "WalloniaF": "bad",
+        "WalloniaG": "bad",
+    }
+
+    # Cleaning for safety
+    epc = df["epc"].astype("string").str.strip()
+
+    # Checks matching value in the epc_map - missing is left as missing
+    df["epc_quality"] = epc.map(epc_map).astype("string")
+
+    # Drop original column
+    df = df.drop(columns=["epc"])
+
+    print("Processed feature 'epc' into 'epc_quality'")
+    return df
+
+def process_flooding_area(df):
+    df = df.copy()
+
+    flooding_area_clean = (
+        df["flooding_area_type"]
+        .astype("string")
+        .str.lower()
+        .str.strip()
+        .str.replace(r"\s+", " ", regex=True)
+        .replace("(information not available)", pd.NA)
+    )
+
+    df["flooding_area_clean"] = flooding_area_clean
+    df = df.drop(columns=["flooding_area_type"])
+
+    print("Processed feature 'flooding_area_type' to 'flooding_area_clean'")
+    return df
+
+def process_binary_missing_values(df):
+    df = df.copy()
+
+    for col in config.BINARY_MISSING_AS_ZERO:
+        df[col] = df[col].fillna(0)
+    print("Processed missing values for binary features")
+    return df
+
+## Select target and features
+def split_target_features(df):
+    target = "price"
+
+    y = df[target]
+    X = df.drop(columns=[target])
+
+    print("Target:", target)
+    print("Feature rows:", len(X))
+    print("Feature columns:", len(X.columns))
+
+    return X, y
+
+## Split train/test
+def split_train_test(X, y):
+
+    X_train, X_test, y_train, y_test = train_test_split(
+        X,
+        y,
+        test_size=0.2,
+        random_state=config.RANDOM_SEED,
+    )
+
+    print("X_train:", X_train.shape)
+    print("X_test:", X_test.shape)
+    print("y_train:", y_train.shape)
+    print("y_test:", y_test.shape)
+
+    return X_train, X_test, y_train, y_test
+
+def main():
+
+    print(f"\n{'=' * 60}")
+    print(" training_base.py ")
+    print(f"\n{'=' * 60}")
+
+    print("[STARTING] Initial setup...")
+    df = load_data()
+    df = remove_exact_duplicates(df)
+    print("[COMPLETED] Initial setup")
+    print("[STARTING] Features processing...")
+    df = engineer_features(df)
+    print("[COMPLETED] Features processing...")
+    print("[STARTING] Target/features split")
+    X, y = split_target_features(df)
+    print("[COMPLETED] Target/features split...")
+    print("[STARTING] Train/test split")
+    X_train, X_test, y_train, y_test = split_train_test(X, y)
+    print("[COMPLETED] Train/test split...")
+
+if __name__ == "__main__":
+    main()
